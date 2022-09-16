604d613b837d1e1dfa5d78e4f30e29b01604b029
shawon ahmmed <shawonahmmed08509@gmail.com>
2022-09-16 17:01:32 +0600

    about, contact, services update api

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 
 app.get('/', (req, res) => {
-    res.send("360 support server")
+  res.send("360 support server")
 })
 
 
@@ -23,36 +23,36 @@ const uploads_folder = './uploads/';
 
 // define storage
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploads_folder)
-    },
-    filename: (req, file, cb) => {
-        const fileExt = path.extname(file.originalname);
-        const fileName = file.originalname.replace(fileExt, "").toLowerCase().split(" ").join("-") + "-" + Date.now();
-        const fullName = fileName + fileExt;
-        cb(null, fullName);
-    }
+  destination: (req, file, cb) => {
+    cb(null, uploads_folder)
+  },
+  filename: (req, file, cb) => {
+    const fileExt = path.extname(file.originalname);
+    const fileName = file.originalname.replace(fileExt, "").toLowerCase().split(" ").join("-") + "-" + Date.now();
+    const fullName = fileName + fileExt;
+    cb(null, fullName);
+  }
 })
 
 
 // file upload function
 const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 3000000,
-    },
-    fileFilter: (req, file, cb) => {
-        if (
-            file.mimetype === "image/webp" ||
-            file.mimetype === "image/png" ||
-            file.mimetype === "image/jpg" ||
-            file.mimetype === "image/jpeg"
-        ) {
-            cb(null, true)
-        } else {
-            cb(new Error("Only webp, png, jpg and jpeg images are allowed"), false)
-        }
+  storage: storage,
+  limits: {
+    fileSize: 3000000,
+  },
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === "image/webp" ||
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/jpeg"
+    ) {
+      cb(null, true)
+    } else {
+      cb(new Error("Only webp, png, jpg and jpeg images are allowed"), false)
     }
+  }
 })
 
 // mongodb connection
@@ -73,102 +73,159 @@ async function run() {
     const servicesCollection = database.collection('services');
     const contactCollection = database.collection('contact');
 
-    app.get('/slider', async (req, res)=>{
-        const cursor = bannerCollection.find({});
-        const result = await cursor.toArray();
-        if ((result.length) === 0) {
-            res.json("No documents found!")
-          } else {
-            res.json(result)
-          }
+    app.get('/slider', async (req, res) => {
+      const cursor = bannerCollection.find({});
+      const result = await cursor.toArray();
+      if ((result.length) === 0) {
+        res.json("No documents found!")
+      } else {
+        res.json(result)
+      }
     })
-    app.get('/features', async (req, res)=>{
-        const cursor = featuresCollection.find({});
-        const result = await cursor.toArray();
-        if ((result.length) === 0) {
-            res.json("No documents found!")
-          } else {
-            res.json(result)
-          }
+    app.get('/features', async (req, res) => {
+      const cursor = featuresCollection.find({});
+      const result = await cursor.toArray();
+      if ((result.length) === 0) {
+        res.json("No documents found!")
+      } else {
+        res.json(result)
+      }
     })
-    app.get('/review', async (req, res)=>{
-        const cursor = reviewCollection.find({});
-        const result = await cursor.toArray();
-        if ((result.length) === 0) {
-            res.json("No documents found!")
-          } else {
-            res.json(result)
-          }
+    app.get('/review', async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const result = await cursor.toArray();
+      if ((result.length) === 0) {
+        res.json("No documents found!")
+      } else {
+        res.json(result)
+      }
     })
-    app.get('/titles', async (req, res)=>{
-        const cursor = titlesCollection.find({});
-        const result = await cursor.toArray();
-        if ((result.length) === 0) {
-            res.json("No documents found!")
-          } else {
-            res.json(result)
-          }
+    app.get('/titles', async (req, res) => {
+      const cursor = titlesCollection.find({});
+      const result = await cursor.toArray();
+      if ((result.length) === 0) {
+        res.json("No documents found!")
+      } else {
+        res.json(result)
+      }
     });
-    app.get('/contact-agent', async (req, res)=>{
-        const cursor = agentContactCollection.find({});
-        const result = await cursor.toArray();
-        if ((result.length) === 0) {
-            res.json("No documents found!")
-          } else {
-            res.json(result)
-          }
+    app.get('/contact-agent', async (req, res) => {
+      const cursor = agentContactCollection.find({});
+      const result = await cursor.toArray();
+      if ((result.length) === 0) {
+        res.json("No documents found!")
+      } else {
+        res.json(result)
+      }
     });
-    app.get('/career-info', async (req, res)=>{
-        const query = {_id: ObjectId("6320ee02edfa774249a946bc")};
-        const cursor = careerInfoCollection.findOne(query);
-        const result = await cursor;
-        if ((result.length) === 0) {
-            res.json("No documents found!")
-          } else {
-            res.json(result)
-          }
+    app.get('/career-info', async (req, res) => {
+      const query = { _id: ObjectId("6320ee02edfa774249a946bc") };
+      const cursor = careerInfoCollection.findOne(query);
+      const result = await cursor;
+      if ((result.length) === 0) {
+        res.json("No documents found!")
+      } else {
+        res.json(result)
+      }
     });
-    app.get('/career-sidebar', async (req, res)=>{
-        const query = {_id: ObjectId("63217c3eedfa7742497b5067")};
-        const cursor = careerSidebarCollection.findOne(query);
-        const result = await cursor;
-        if ((result.length) === 0) {
-            res.json("No documents found!")
-          } else {
-            res.json(result)
-          }
+    app.get('/career-sidebar', async (req, res) => {
+      const query = { _id: ObjectId("63217c3eedfa7742497b5067") };
+      const cursor = careerSidebarCollection.findOne(query);
+      const result = await cursor;
+      if ((result.length) === 0) {
+        res.json("No documents found!")
+      } else {
+        res.json(result)
+      }
     });
-    app.get('/about', async (req, res)=>{
-        const query = {_id: ObjectId("63218480edfa77424987c834")};
-        const cursor = aboutCollection.findOne(query);
-        const result = await cursor;
-        if ((result.length) === 0) {
-            res.json("No documents found!")
-          } else {
-            res.json(result)
-          }
+    app.get('/about', async (req, res) => {
+      const query = { _id: ObjectId("63218480edfa77424987c834") };
+      const cursor = aboutCollection.findOne(query);
+      const result = await cursor;
+      if ((result.length) === 0) {
+        res.json("No documents found!")
+      } else {
+        res.json(result)
+      }
     });
-    app.get('/services', async (req, res)=>{
-        const query = {_id: ObjectId("63232897edfa774249098284")};
-        const cursor = servicesCollection.findOne(query);
-        const result = await cursor;
-        if ((result.length) === 0) {
-            res.json("No documents found!")
-          } else {
-            res.json(result)
-          }
+    app.put('/update-about', async (req, res)=>{
+      const data = req.body;
+      const {_id, aboutTitle, aboutDesc, aboutAuthor} = data;
+      const filter = {_id : ObjectId(_id)}
+      const updateDoc = {
+        $set: {
+            aboutTitle,
+            aboutDesc,
+            aboutAuthor
+        }
+      };
+      const result = await aboutCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    })
+    app.get('/services', async (req, res) => {
+      const query = { _id: ObjectId("63232897edfa774249098284") };
+      const cursor = servicesCollection.findOne(query);
+      const result = await cursor;
+      if ((result.length) === 0) {
+        res.json("No documents found!")
+      } else {
+        res.json(result)
+      }
     });
-    app.get('/contact', async (req, res)=>{
-        const query = {_id: ObjectId("632330f3edfa77424916eb67")};
-        const cursor = contactCollection.findOne(query);
-        const result = await cursor;
-        if ((result.length) === 0) {
-            res.json("No documents found!")
-          } else {
-            res.json(result)
-          }
+
+    app.put('/update-services', async (req, res)=>{
+      const data = req.body;
+      const {_id, serviceOneTitle, serviceOnedesc, serviceTwoTitle, serviceTwoDesc, serviceThreeTitle, serviceThreedesc} = data;
+      const filter = {_id: ObjectId(_id)};
+      const updateDoc = {
+        $set:{
+            serviceOneTitle,
+            serviceOnedesc,
+            serviceTwoTitle,
+            serviceTwoDesc,
+            serviceThreeTitle,
+            serviceThreedesc
+        }
+      };
+      const result = await servicesCollection.updateOne(filter, updateDoc);
+      res.json(result)
     });
-    
+
+    app.get('/contact', async (req, res) => {
+      const query = { _id: ObjectId("632330f3edfa77424916eb67") };
+      const cursor = contactCollection.findOne(query);
+      const result = await cursor;
+      if ((result.length) === 0) {
+        res.json("No documents found!")
+      } else {
+        res.json(result)
+      }
+    });
+
+    // update contact data
+    app.put('/update-contact', async (req, res) => {
+      const data = req.body;
+      const { _id, contactTitle, optionOneTitle, optionTwoTitle, optionThreeTitle, optionFourTitle, optionOnedesc, optionTwoDesc, optionThreedesc, optionFourdesc } = data;
+      const filter = { _id: ObjectId(_id) };
+      const updateDoc = {
+        $set: {
+          contactTitle,
+          optionOneTitle,
+          optionTwoTitle,
+          optionThreeTitle,
+          optionFourTitle,
+          optionFourdesc,
+          optionOnedesc,
+          optionThreedesc,
+          optionTwoDesc
+        },
+      };
+      const result = await contactCollection.updateOne(filter, updateDoc);
+
+      console.log(result);
+      res.json(result)
+    })
+
     app.use((err, req, res, next) => {
       if (err) {
         res.status(500).send(err.message)
@@ -185,5 +242,5 @@ run().catch(console.dir);
 
 
 app.listen(port, () => {
-    console.log(`listening to the port:  ${port}`)
+  console.log(`listening to the port:  ${port}`)
 })
